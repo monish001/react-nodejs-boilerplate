5b8329b09b3afe0f5c2fabbac05c1517dea59dab
Monish Gupta <monish.gupta1@gmail.com>
2018-05-06 17:48:01 +0530

    minor refactor

--- client/src/components/Home/index.js
+++ client/src/components/Home/index.js
@@ -1,8 +1,9 @@
-import React, { Component } from "react";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import "./style.css";
-import Nav from "../Nav";
-import EnsureLoggedInContainer from "../../containers/EnsureLoggedInContainer";
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import './style.css';
+import Nav from '../Nav';
+import EnsureLoggedInContainer from '../../containers/EnsureLoggedInContainer';
+
 class Home extends Component {
   render() {
     let distanceThisWeek; //= 50;
