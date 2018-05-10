0d2a1f8d370edd45ae13326fd0c7db306fbfdcee
Monish Gupta <monish.gupta1@gmail.com>
2018-05-11 01:39:06 +0530

    view records API integration done.

--- client/src/components/ViewRecords/index.js
+++ client/src/components/ViewRecords/index.js
@@ -3,35 +3,62 @@ import { Link } from 'react-router-dom';
 import './style.css';
 import EnsureLoggedInContainer from '../../containers/EnsureLoggedInContainer';
 import * as Repository from '../../repositories/user';
+import * as UserRecordsRepository from '../../repositories/user-record';
 import Nav from '../Nav';
 
-class Home extends Component {
+class ViewRecords extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      records: [],
+    };
+  }
+
+  componentWillMount() {
+    // TODO loader
+    UserRecordsRepository
+      .get()
+      .then((response) => {
+        console.log(response.data);
+        this.setState({
+          records: response.data,
+        });
+      }).catch(err => console.error(err));
+  }
+
   render() {
-    const data = [
-      {
-        date: '12 jan 2018',
-        DistanceInMiles: 2,
-        TimeDurationInMinutes: 60,
-        CreatedTimeStamp: 1515924253213,
-        LastModifiedTimeStamp: 1523958286105,
-      },
-    ];
+    // CreatedTimeStamp
+    // :
+    // 1515924253213
+    // DistanceInMiles
+    // :
+    // 61.61
+    // LastModifiedTimeStamp
+    // :
+    // 1523958286105
+    // TimeDurationInMinutes
+    // :
+    // 61
+    // UserId
+    // :
+    // "116fa1c6-82aa-4288-afea-c1d9477fb05d"
+    const { records } = this.state;
     const UserId = Repository.getUserId();
     let content;
 
-    if (data.length > 0) {
+    if (records.length > 0) {
       content = (
         <ul className="no-list-style-type">
           <li key={-1}>
             Date | Distance (in miles) | Time (in mins) | Actions
           </li>
-          {data.map((row) => {
-            const { CreatedTimeStamp, DistanceInMiles, TimeDurationInMinutes } = row;
+          {records.map((record) => {
+            const { CreatedTimeStamp, DistanceInMiles, TimeDurationInMinutes } = record;
             const key = `${CreatedTimeStamp}::${UserId}`;
             return (
               <li key={key}>
                 {`${new Date(CreatedTimeStamp)} | ${DistanceInMiles} | ${TimeDurationInMinutes} | `}
-                <Link to="/records/edit">Edit</Link>{' | '}
+                <Link to={`/records/edit/${CreatedTimeStamp}`}>Edit</Link>{' | '}
                 <a
                   href="#"
                   onClick={() =>
@@ -62,5 +89,4 @@ class Home extends Component {
     );
   }
 }
-
-export default Home;
+export default ViewRecords;
--- client/src/repositories/user-record.js
+++ client/src/repositories/user-record.js
@@ -6,16 +6,13 @@ import * as BaseRepository from './base';
 
 /**
  * get
- * @param {object} args
  */
-export function get(args) {
+export function get() {
   // See https://stackoverflow.com/a/39333479/989139 for help on syntax
   const request = {
     UserId: UserRepository.getUserId(),
-    DistanceInMiles: args.distance,
-    TimeDurationInMinutes: args.time,
   };
-  return http.get(`/api/users/${UserRepository.getUserId()}`, request, { headers: BaseRepository.getHeaders() });
+  return http.get(`/api/users/${UserRepository.getUserId()}/records`, request, { headers: BaseRepository.getHeaders() });
 }
 
 /**
