ee392d452ee15f8e9bb3099dd9bd80238bc0ad95
Monish Gupta <monish.gupta1@gmail.com>
2018-06-03 19:07:29 +0530

    Basic code added to support redux

--- client/src/components/App/index.js
+++ client/src/components/App/index.js
@@ -1,30 +1,36 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import Home from '../Home';
 import CreateRecord from '../CreateRecord';
 import ViewRecords from '../ViewRecords';
 import EditRecord from '../EditRecord';
 import './style.css';
-
 import Login from '../Login';
 import Register from '../Register';
+import reducer from '../../reducers';
+
+const store = createStore(reducer);
 
 const App = () => (
-  <div className="App">
-    <header className="App-header">
-      <h1>Jogger App</h1>
-    </header>
-    <Router>
-      <div>
-        <Route exact path="/" component={Home} />
-        <Route path="/login" component={Login} />
-        <Route path="/register" component={Register} />
-        <Route path="/records/create" component={CreateRecord} />
-        <Route path="/records/view" component={ViewRecords} />
-        <Route path="/records/edit/:CreatedTimeStamp" component={EditRecord} />
-      </div>
-    </Router>{' '}
-  </div>
+  <Provider store={store}>
+    <div className="App">
+      <header className="App-header">
+        <h1>Jogger App</h1>
+      </header>
+      <Router>
+        <div>
+          <Route exact path="/" component={Home} />
+          <Route path="/login" component={Login} />
+          <Route path="/register" component={Register} />
+          <Route path="/records/create" component={CreateRecord} />
+          <Route path="/records/view" component={ViewRecords} />
+          <Route path="/records/edit/:CreatedTimeStamp" component={EditRecord} />
+        </div>
+      </Router>{' '}
+    </div>
+  </Provider>
 );
 
 export default App;
